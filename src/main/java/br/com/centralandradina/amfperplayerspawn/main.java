6f34b541e5403cf747da1aa78468cc5147a106da
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-12-29 15:07:14 -0300

    added /spawn command

--- src/main/java/br/com/centralandradina/amfperplayerspawn/main.java
+++ src/main/java/br/com/centralandradina/amfperplayerspawn/main.java
@@ -9,6 +9,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -63,8 +65,11 @@ public class main extends JavaPlugin implements Listener
 			
 
 			// look for a new spawn area
-			this.spawnLocation = newSpawnLocation(player.getWorld());
-			getLogger().info("Found location for " + playerName + " at " + this.spawnLocation.getX() + ", " + this.spawnLocation.getY() + ", " + this.spawnLocation.getZ());
+			Location location = newSpawnLocation(player.getWorld());
+			getLogger().info("Found location for " + playerName + " at " + location.getX() + ", " + location.getY() + ", " + location.getZ());
+
+			// get location
+			this.spawnLocation = new Location(location.getWorld(), location.getX()+0.5, location.getY()+0.5, location.getZ()+0.5);
 
 			// save the location for next 
 			FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
@@ -91,12 +96,17 @@ public class main extends JavaPlugin implements Listener
 			World  world = getServer().getWorld(config.getString("spawn.world"));
 			
 			// get location
-			this.spawnLocation = new Location(world, config.getInt("spawn.x"), config.getInt("spawn.y"), config.getInt("spawn.z"));
+			this.spawnLocation = new Location(world, config.getInt("spawn.x")+0.5, config.getInt("spawn.y")+0.5, config.getInt("spawn.z")+0.5);
 
 		}
 
 	}
 
+	/**
+	 * find new spawn location
+	 * @param world
+	 * @return
+	 */
 	public Location newSpawnLocation(World world)
 	{
 		getLogger().info("Finding new spawn location...");
@@ -115,6 +125,8 @@ public class main extends JavaPlugin implements Listener
 		int posY = world.getHighestBlockYAt(posX, posZ);
 		while(posY >= 0)
 		{
+			// @todo verify if this place has protection (redprotection and griefprevent)
+
 			if(world.getBlockAt(posX, posY, posZ).getType() == Material.WATER) {
 				return newSpawnLocation(world);
 			}
@@ -122,6 +134,7 @@ public class main extends JavaPlugin implements Listener
 				return newSpawnLocation(world);
 			}
 			else if(world.getBlockAt(posX, posY, posZ).getType() != Material.AIR) {
+				posY++; // back to the previous block position
 				return new Location(world, posX, posY, posZ);
 			}
 
@@ -129,6 +142,46 @@ public class main extends JavaPlugin implements Listener
 		}
 
 		return new Location(world, 0, 0, 0);
+	}
+
+	/**
+	 * commands
+	 * 
+	 * @todo translate
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+
+		String commandName = cmd.getName().toLowerCase();
+
+		// verify if the command was executed by a player
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Execute this command as a player");
+			return false;
+		}
+		Player player = (Player)sender;
+
+		// /spawn command
+		if(commandName.equals("spawn")) {
+
+			// if the command has no arguments, send player to their spawn
+			if(args.length == 0) {
+    			
+				// verify if has permission
+				if(!sender.hasPermission("amf.perplayerspawn.spawn")) {
+					sender.sendMessage("You cannot to that!");
+    				return false;
+				}
+
+				// teleport player
+				player.teleport(this.spawnLocation);
+    			return false;
+    		}
+
+		}
+
+		return false;
 	}
 
 }
